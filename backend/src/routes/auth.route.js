afd2df718bf42064c3ee608b23d161b11aab4dd5
Cathal McHale <cm@tus.ie>
2025-09-02 19:12:46 +0100

    API endpoints working

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,6 +7,8 @@ const router = express.Router()
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+// if the user is logged in they can update profile
+router.put("/update-profile", protectRoute, updateProfile)
 
 
 
