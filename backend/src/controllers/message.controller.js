cf4e2d47a0bb1c07980d88c00f8b2fd891cde3a2
Cathal McHale <cm@tus.ie>
2025-09-08 15:33:32 +0100

    getMessages function complete

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,6 @@
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
+
 export const getUsersForSidebar = async(req, res) => {
     try {
         const loggedInUserId = req.user._id;
@@ -10,4 +12,25 @@ export const getUsersForSidebar = async(req, res) => {
         console.log("Error in getUsersForSidebar", error.message)
         res.status(500),json({error: "Internal server error"})
     }
+}
+
+export const getMessages = async(req, res) =>{
+    try {
+        const {id:userToChatId} = req.params
+        const senderId = req.user._id;
+
+        //find all messages from sender and reciever in one chat
+        const messages = await Message.find({
+            $or:[
+                {senderId:senderId, recieverId:userToChatId},
+
+            ]
+        })
+
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log("Error in getMessages controller", error.message)
+        res.status(500).json({error: "Internal server error"})
+    }
+
 }
