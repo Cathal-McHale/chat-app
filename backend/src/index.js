36190a43300af58188aaf22a1761a9e0cd3386d0
Cathal McHale <cm@tus.ie>
2025-09-08 17:39:08 +0100

    tailwind inital set up

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
+import './index.css'
 
 const app = express();
 dotenv.config()
