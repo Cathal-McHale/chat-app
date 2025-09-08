816f1093e97f6d9403c9555cde5323eb8f0da51b
Cathal McHale <cm@tus.ie>
2025-09-08 15:39:18 +0100

    send message function complete

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -33,4 +34,36 @@ export const getMessages = async(req, res) =>{
         res.status(500).json({error: "Internal server error"})
     }
 
+}
+
+export const sendMessgae = async(req, res)=>{
+    try {
+        const {text, image} = req.body
+        const {id: recieverId} = req.params
+        const senderId = req.user._id
+
+        let imageURL;
+        if(image){
+            //upload base 64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            //assign to image URL
+            imageURL = uploadResponse.secure_url
+        }
+
+        const newMessage = new Message({
+            senderId,
+            recieverId,
+            text,
+            image: imageURL,
+        })
+
+        await newMessage.save();
+
+
+        //add realtime functionailty later - Socket.io
+        res.status(201).json(newMessage)
+    } catch (error) {
+        console.log("Error in sendMessage controller", error.message)
+        res.status(500).json({error: "Internal server error"})
+    }
 }
