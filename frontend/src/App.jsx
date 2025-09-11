4aecb55e4899b5cb48c5c1a9103eb14e46b9fcd2
Cathal McHale <cm@tus.ie>
2025-09-11 11:54:00 +0100

    added protected routes and axios initial set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
-import "./index.css";
-import NavBar from './components/NavBar';
+import React, { useEffect } from 'react'
+//import "./index.css";
+// Duplicate import removed
 import { Routes, Route } from 'react-router-dom';
 import SettingsPage from './pages/SettingsPage';
 import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
+import NavBar from "./components/NavBar";
+import { useAuthStore } from './store/useAuthStore';
+import { Loader } from 'lucide-react';
 
 const App = () => {
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth])
+
+  console.log({authUser})
+  if(isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
   return (
     <div>
-      <Navbar />
+      <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
   );
