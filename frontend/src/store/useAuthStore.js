151fb19aa88bfbe031d6addb19362c82c08b3e7a
Cathal McHale <cm@tus.ie>
2025-09-21 17:00:54 +0100

    home page layout and sidebar added with chatcontainer

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -14,6 +14,7 @@ export const useAuthStore = create((set) => ({
     isLoggingIn: false,
     isUpdatingProfile: false,
     isCheckingAuth: true,
+    onlineUsers: [],
 
     checkAuth: async () => {
         try {
