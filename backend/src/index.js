d99f4ce96780fd2e7a1420bbf1c735f1f473ada0
Cathal McHale <cm@tus.ie>
2025-09-21 18:27:33 +0100

    fixed error for express 5.1

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,9 +33,9 @@ app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV ==="production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../frontend/", "dist", "index.html"))
-  })
+ app.get(/.*/, (_req, res) => {
+   res.sendFile(path.join(clientBuildPath, "index.html"));
+ });
 }
 
 const server = app.listen(PORT, () => {
